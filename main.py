5c17c5e2406bb9be403421857cfb20645417d586
alikhabazian <52274611+alikhabazian@users.noreply.github.com>
2022-10-16 16:24:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ proxy_port=8080
 bridge_port_input=1210
 message_id=67
 name_server="Proxy server"
+sleep_time=300 # 5 minutes
+manager_telegram_id=68463073939
+
 req=telegram.utils.request.Request(proxy_url=f"socks5h://127.0.0.1:{proxy_port}")
 bot=telegram.Bot(API_TOKEN,request =req)
-#bot.send_message("@rahbazkoon","s")
+
+
 small_command=f"tcp port {bridge_port_input}"
 some_command = 'sudo tshark -T fields -e ip.src   -f "'+small_command+'" | head -1000 |sort -u'
 while(True):
@@ -29,5 +33,5 @@ while(True):
         I_time = datetime.now(Iran)
         bot.editMessageText(f"I am {name_server}\n{I_time.strftime('%Y-%m-%d_%H:%M')} \nThere are {leno} online:\n {output1}",chat_id=user_id,message_id=message_id)
     except Exception as e:
-        print(e)
-    time.sleep(300)
+        bot.send_message(manager_telegram_id,f"error is:\n{str(e)}")
+    time.sleep(sleep_time)
