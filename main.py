58ab375e2cb2daa28c8a2925aed85c0dedb09e5c
alikhabazian <52274611+alikhabazian@users.noreply.github.com>
2022-10-16 16:19:10 +0330

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,25 @@ from pytz import timezone
 Iran = timezone('Iran')
 API_TOKEN=config.API_TOKEN
 user_id="@rahbazkoon"
-req=telegram.utils.request.Request(proxy_url="socks5h://127.0.0.1:8080")
+proxy_port=8080
+bridge_port_input=1210
+message_id=67
+name_server="Proxy server"
+req=telegram.utils.request.Request(proxy_url=f"socks5h://127.0.0.1:{proxy_port}")
 bot=telegram.Bot(API_TOKEN,request =req)
 #bot.send_message("@rahbazkoon","s")
-some_command = 'sudo tshark -T fields -e ip.src   -f "tcp port 1210" | head -1000 |sort -u'
+small_command=f"tcp port {bridge_port_input}"
+some_command = 'sudo tshark -T fields -e ip.src   -f "'+small_command+'" | head -1000 |sort -u'
 while(True):
-    p = subprocess.Popen(some_command, stdout=subprocess.PIPE, shell=True)
-    p_status = p.wait()
-    (output, err) = p.communicate()
-    output1=output.decode('UTF-8')
-    print(output1.splitlines())
-    leno=len(output1.splitlines())
-    I_time = datetime.now(Iran)
-    bot.editMessageText(f"{I_time.strftime('%Y-%m-%d_%H:%M')} \nThere are {leno} online:\n {output1}",chat_id=user_id,message_id=67)
+    try:
+        p = subprocess.Popen(some_command, stdout=subprocess.PIPE, shell=True)
+        p_status = p.wait()
+        (output, err) = p.communicate()
+        output1=output.decode('UTF-8')
+        print(output1.splitlines())
+        leno=len(output1.splitlines())
+        I_time = datetime.now(Iran)
+        bot.editMessageText(f"I am {name_server}\n{I_time.strftime('%Y-%m-%d_%H:%M')} \nThere are {leno} online:\n {output1}",chat_id=user_id,message_id=message_id)
+    except Exception as e:
+        print(e)
     time.sleep(300)
