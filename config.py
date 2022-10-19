206060b3723a6e273af150f67509285a5707a7c1
alikhabazian <khabaziana@gmail.com>
2022-10-19 17:38:46 +0000

    rasoul ghor ghor

--- config.py
+++ config.py
@@ -1,9 +1,24 @@
 API_TOKEN="5769251755:AAEN2M-CeDT2XjmL4c2ZWrWyYLA9Lr6ebdo"
 time_zone='Iran'
-channel_id='@rahbazkoon'
+channel_id='-1001842583212'
 proxy_port=8080
 bridge_port_input=1210
-message_id=67
-name_server="Proxy server"
+message_id=2
+name_server="Amin"
 sleep_time=300 #5 minutes
 manager_telegram_id=68463073939
+limit=""
+message="""
+سرور امین بذرگر (فعال✅)
+
+ss://YWVzLTEyOC1nY206c2hhbHZhcnNvcmF0aTIxKkAxOTUuMTEwLjM4LjE5MToxMjEw #Amin_Bazrgar
+
+Note: 
+    IR IP: 195.110.38.191 🇮🇷
+    DE IP: 159.69.180.138 🇩🇪
+
+    Hamrah-e Aval ✅
+    Rightel ✅
+    Irancel ✅
+    Google Services ✅
+"""
--- main.py
+++ main.py
@@ -15,6 +15,8 @@ message_id=config.message_id
 name_server=config.name_server
 sleep_time=config.sleep_time
 manager_telegram_id=config.manager_telegram_id
+limit=config.limit
+message=config.message
 
 req=telegram.utils.request.Request(proxy_url=f"socks5h://127.0.0.1:{proxy_port}")
 bot=telegram.Bot(API_TOKEN,request =req)
@@ -31,7 +33,7 @@ while(True):
         print(output1.splitlines())
         leno=len(output1.splitlines())
         I_time = datetime.now(tz)
-        bot.editMessageText(f"I am {name_server}\n{I_time.strftime('%Y-%m-%d_%H:%M')} \nThere are {leno} online:\n {output1}",chat_id=channel_id,message_id=message_id)
+        bot.editMessageText(f"{message} \n -----------\n{name_server} server {I_time.strftime('%Y/%m/%d %H:%M')} \nOnline users: {leno}\nServer limit: {limit}",chat_id=channel_id,message_id=message_id,timeout=30)
     except Exception as e:
-        bot.send_message(manager_telegram_id,f"error is:\n{str(e)}")
+        bot.send_message(manager_telegram_id,f"error is:\n{str(e)}",timeout=1000)
     time.sleep(sleep_time)
